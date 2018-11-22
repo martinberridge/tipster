cbe445865028b2fbc935ddcd95219f0b2d792f8b
user1 <martin.berridge@gmail.com>
2018-11-22 20:52:06 +0000

    mongo db pipeline for form

--- data/form/form/pipelines.py
+++ data/form/form/pipelines.py
@@ -1,11 +1,27 @@
-# -*- coding: utf-8 -*-
+import pymongo
 
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+from scrapy.conf import settings
+from scrapy.exceptions import DropItem
+import logging
 
+class MongoDBPipeline(object):
 
-class FormPipeline(object):
+    def __init__(self):
+        connection = pymongo.MongoClient(
+            settings['MONGODB_SERVER'],
+            settings['MONGODB_PORT']
+        )
+        db = connection[settings['MONGODB_DB']]
+        self.collection = db[settings['MONGODB_COLLECTION']]
+
     def process_item(self, item, spider):
+        logging.log(logging.DEBUG, "process Item")
+        valid = True
+        for data in item:
+            if not data:
+                valid = False
+                raise DropItem("Missing {0}!".format(data))
+        if valid:
+            logging.log(logging.DEBUG, "inserting item")
+            self.collection.insert(dict(item))
         return item
