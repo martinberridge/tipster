68d0cae0b977a01ea233b2682c532ba478031abe
user1 <martin.berridge@gmail.com>
2018-11-22 21:21:51 +0000

    meetings mongo db pipe

--- data/meetings/meetings/pipelines.py
+++ data/meetings/meetings/pipelines.py
@@ -4,8 +4,30 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import pymongo
 
+from scrapy.conf import settings
+from scrapy.exceptions import DropItem
+import logging
 
-class MeetingsPipeline(object):
+class MongoDBPipeline(object):
+
+    def __init__(self):
+        connection = pymongo.MongoClient(
+            settings['MONGODB_SERVER'],
+            settings['MONGODB_PORT']
+        )
+        db = connection[settings['MONGODB_DB']]
+        self.collection = db[settings['MONGODB_COLLECTION']]
+
     def process_item(self, item, spider):
+        logging.log(logging.DEBUG, "process Item")
+        valid = True
+        for data in item:
+            if not data:
+                valid = False
+                raise DropItem("Missing {0}!".format(data))
+        if valid:
+            logging.log(logging.DEBUG, "inserting item")
+            self.collection.insert(dict(item))
         return item
--- data/meetings/meetings/settings.py
+++ data/meetings/meetings/settings.py
@@ -88,3 +88,9 @@ ROBOTSTXT_OBEY = True
 #HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+ITEM_PIPELINES = { 'meetings.pipelines.MongoDBPipeline':300, }
+
+MONGODB_SERVER = "localhost"
+MONGODB_PORT = 27017
+MONGODB_DB = "tipster"
+MONGODB_COLLECTION = "meetings"
